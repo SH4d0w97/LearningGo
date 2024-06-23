fe38e93ee48543fbf656745b8c6e4cb5019fe30a
soorajsr97 <soorajsr97@gmail.com>
2024-06-23 07:27:39 +0000

    added comments

--- Learning-Go/cmd/myapp/main.go
+++ Learning-Go/cmd/myapp/main.go
@@ -12,14 +12,13 @@ import (
 var Pl = fmt.Println
 
 func main() {
-	Pl("Enter your name : ") // Prompt the user to enter their name
-	reader := bufio.NewReader(os.Stdin) // Create a new reader for standard input
+	Pl("Enter your name : ")             // Prompt the user to enter their name
+	reader := bufio.NewReader(os.Stdin)  // Create a new reader for standard input
 	name, err := reader.ReadString('\n') // Read the user's input until a newline character
-	
-	if err == nil { 
+
+	if err == nil {
 		Pl(mypkg.Hello(name)) // If there's no error, greet the user using the mypkg.Hello function
 	} else {
 		log.Fatal(err) // Otherwise, log the error and exit the program
 	}
-	
 }
